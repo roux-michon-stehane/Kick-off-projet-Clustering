1f9402f8cb80407cd7608387d83aac2fb1b2e307
Ulquileon <pierre-alexis.lebair@epitech.eu>
2023-04-02 15:48:42 +0200

    KMeans class added labels

--- kmeans.py
+++ kmeans.py
@@ -1,13 +1,14 @@
 import numpy as np
 from scipy.spatial.distance import cdist
 
-class KMeans:
+class myKMeans:
     def __init__(self, k=8, init='random', max_iter=300, tol=1e-4):
         self.k = k
         self.init = init
         self.max_iter = max_iter
         self.tol = tol
         self.centroids = None
+        self.labels = None
 
     def fit(self, X):
         n_samples, n_features = X.shape
@@ -26,12 +27,13 @@ class KMeans:
 
         for _ in range(self.max_iter):
             distances = cdist(X, self.centroids)
-            labels = np.argmin(distances, axis=1)
+            self.labels = np.argmin(distances, axis=1)
             new_centroids = np.zeros((self.k, n_features))
             for i in range(self.k):
-                new_centroids[i] = X[labels == i].mean(axis=0)
+                new_centroids[i] = X[self.labels == i].mean(axis=0)
             if np.abs(new_centroids - self.centroids).sum() < self.tol:
                 break
+
             self.centroids = new_centroids
 
     def predict(self, X):
